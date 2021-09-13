3536db05ffa4f8e78c6086a6e2a88b53d830cd79
gabip1 <75934119+gabip1@users.noreply.github.com>
2021-09-14 01:25:10 +0200

    Update MoviesService.js

--- src/services/MoviesService.js
+++ src/services/MoviesService.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const MOVIES_API_BASE_URL = "http://backend-movie-app.herokuapp.com/api/v1/movies"
+const MOVIES_API_BASE_URL = "https://backend-movie-app.herokuapp.com/api/v1/movies"
 
 class MoviesService {
 
